20ae228268c5852846f501b88d9cf3a06a577fe1
AriBaderkhan <ari99baderkhan@gmail.com>
2026-01-03 15:22:02 +0300

    fix super_doctor validate

--- validates/authValidate.js
+++ validates/authValidate.js
@@ -9,7 +9,7 @@ const schemaRegister = Joi.object({
     phone: Joi.string().optional(),
     address: Joi.string().optional(),
     room: Joi.when('role', {
-        is: 'doctor',
+        is: ['doctor','super_doctor'],
         then: Joi.number().required(),
         otherwise: Joi.forbidden()
     }),
